6de097f3b87f1e62a9ead4417d7c83a3b0195c28
sakuma-shin <k023g0084@g.neec.ac.jp>
2024-04-22 13:15:52 +0900

    1-2

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
-    int[] map = { 0, 0, 1, 0, 2, 0, 2,0 };
+
+    public GameObject playerPreFab;
+    int[,] map;
+
+    GameObject[,] field; //ゲーム管理用の配列
+
+    GameObject obj;
     
+
     // Start is called before the first frame update
 
     void Start()
     {
+        map = new int[,]{
+        { 0, 0, 1, 0, 0 },
+        { 0, 0, 0, 0, 0, },
+        { 0, 0, 0, 0, 0, }
+        };
+
+        //フィールドのサイズ決定
+        field = new GameObject[map.GetLength(0), map.GetLength(1)];
+
+        for (int y = 0; y < map.GetLength(0); y++)
+        {
+            for (int x = 0; x < map.GetLength(1); x++)
+            {
+                if (map[y, x] == 1)
+                {
+                    field[y, x] = Instantiate(
+                    playerPreFab,
+                    new Vector3(x, map.GetLength(0) - 1.0f - y, 0.0f),
+                    Quaternion.identity
+                    );
+                }
+
+            }
+        }
         string debugText = "";
 
-        PrintArray();
+        for (int y = 0; y < map.GetLength(0); y++)
+        {
+            for (int x = 0; x < map.GetLength(1); x++)
+            {
+                debugText += map[y, x].ToString() + ",";
+            }
+            debugText += "\n";
+        }
+
+
+
+        // PrintArray();
         //デバッグログの出力
         //Debug.Log("Hello world\n");
 
@@ -27,7 +70,7 @@ public class GameManagerScript : MonoBehaviour
         {
             //1.をここから記述
             //見つからなかったときの為に-1で初期化
-            int playerIndex = GetPlayerIndex();
+            //int playerIndex = GetPlayerIndex();
             ////要素数はmap.Lengthで取得
             //for(int i = 0;i < map.Length;i++) {
             //    if (map[i]==1){
@@ -49,55 +92,64 @@ public class GameManagerScript : MonoBehaviour
             //    }
             //}
 
-            MoveNumber(1, playerIndex, playerIndex + 1);
-            PrintArray();
+            //MoveNumber(1, playerIndex, playerIndex + 1);
+            //PrintArray();
 
         }
     }
 
-    void PrintArray()
-    {
-        string debugText = "";
-        for (int i = 0; i < map.Length; i++)
-        {
-            debugText += map[i].ToString() + ",";
-        }
-        Debug.Log(debugText);
-    }
 
-    int GetPlayerIndex()
+    //    void PrintArray()
+    //    {
+    //        string debugText = "";
+    //        for (int i = 0; i < map.Length; i++)
+    //        {
+    //            debugText += map[i].ToString() + ",";
+    //        }
+    //        Debug.Log(debugText);
+    //    }
+
+    Vector2Int GetPlayerIndex()
     {
-        for (int i = 0; i < map.Length; i++)
+        for (int y = 0; y < field.GetLength(0); y++)
         {
-            if (map[i] == 1)
+            for (int x = 0; x < field.GetLength(1); x++)
             {
-                return i;
+                if (field[y, x] == null)
+                {
+                    continue;
+                }
+                if (obj.tag=="player")
+                {
+                    return new Vector2Int(x,y);
+                }
             }
         }
-        return -1;
+        return new Vector2Int(-1, -1);
     }
 
-    bool MoveNumber(int number, int moveFrom, int moveTo)
-    {
-        if (moveTo < 0 || moveTo >= map.Length)
-        {
-            return false;
-        }
+    //    bool MoveNumber(int number, int moveFrom, int moveTo)
+    //    {
+    //        if (moveTo < 0 || moveTo >= map.Length)
+    //        {
+    //            return false;
+    //        }
 
-        if (map[moveTo]==2) {
-            //どの方向へ移動するかを算出
-            int velocity = moveTo - moveFrom;
-            /*プレイヤーの移動先から、さらに先へ2(箱)を移動させる
-             箱の移動処理,MoveNumberメソッド内でMoveNumberメソッドを呼び
-            処理が再起している。移動可不可をboolで記録*/
-            bool success = MoveNumber(2, moveTo, moveTo + velocity);
-            //もし箱が移動失敗したら、プレイヤーの移動も失敗
-            if(!success){
-                return false;
-            }
-        }
-        map[moveTo] = number;
-        map[moveFrom] = 0;
-        return true;
-    }
+    //        if (map[moveTo]==2) {
+    //            //どの方向へ移動するかを算出
+    //            int velocity = moveTo - moveFrom;
+    //            /*プレイヤーの移動先から、さらに先へ2(箱)を移動させる
+    //             箱の移動処理,MoveNumberメソッド内でMoveNumberメソッドを呼び
+    //            処理が再起している。移動可不可をboolで記録*/
+    //            bool success = MoveNumber(2, moveTo, moveTo + velocity);
+    //            //もし箱が移動失敗したら、プレイヤーの移動も失敗
+    //            if(!success){
+    //                return false;
+    //            }
+    //        }
+    //        map[moveTo] = number;
+    //        map[moveFrom] = 0;
+    //        return true;
+    //    }
+    //}
 }
